12474c21ed8fe985df00cfaa267fd5034c6be6a3
Develkone <805310424@qq.com>
2021-10-26 16:56:15 +0800

    强制用户认证

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,5 @@ Route::post('paswword/reset', 'Auth\ForgotPasswordController@reset')->name('pass
 
 // Email认证相关路由
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verity')->name('verification.verify');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
